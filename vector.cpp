729165b7c91923d0ad5892b31980ab7c6b1bc115
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 16:12:57 +0600

    vector declaration, initialization and print

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>nasim;
+    nasim.push_back(3);
+    nasim.push_back(9);
+    nasim.push_back(49);
+    nasim.push_back(10);
+    cout<<nasim[2];
+}
