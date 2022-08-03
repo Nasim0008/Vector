12ce273b5d10657a5e28dcd7ee2480d3d2ac9302
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 17:00:53 +0600

    Initialize the values when the vector created

---
+++ declaration and initialization together.cpp
@@ -0,0 +1,15 @@
+//Initialize the values when the vector created
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>nasim ={10,52,84,1,2,36};//declaration and initialization
+    for(int i=0;i<nasim.size();i++)
+    {
+        cout<<nasim[i]<< " ";
+    }
+
+    return 0;
+}
