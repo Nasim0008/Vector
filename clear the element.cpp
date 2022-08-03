5dc101326220f1a209ed6495f54c3c60669a9f79
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 17:14:18 +0600

    Clear all the element which is initialize before

---
+++ clear the element.cpp
@@ -0,0 +1,15 @@
+//Clear all the element which is initialize before
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>nasim ={10,52,84,1,2,36};//declaration and initialization
+    nasim.clear();//vector name.clear()//this function clear all the elements which is initialize before
+
+    cout<<nasim.size();//size=0 means empty thats means clear
+
+    return 0;
+}
+
