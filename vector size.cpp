d978190fb1a24852b428bc81d83e84608cddbca3
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-03 16:30:19 +0600

    find the size of vector by using the size() function

---
+++ vector size.cpp
@@ -0,0 +1,21 @@
+
+//vector size finding function
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //vector declaration
+    vector<int>nasim; //vector keyword<data type>vector name; (vector name can be anything);
+    //vector initialization
+    nasim.push_back(3);
+    nasim.push_back(9);
+    nasim.push_back(49);
+    nasim.push_back(10);
+
+
+    cout<<nasim.size();//vector_name.size()//finding the size of vector
+
+    return 0;
+}
+
