c87ed9b4421f579e0e0f6ce2034ea38dd703dfd5
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-04 10:29:28 +0600

    create the memory size and take user input and print the value

---
+++ vector as like array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>nasim;
+    nasim.resize(5);
+    for(int i=0;i<nasim.size();i++)
+    {
+        cin>>nasim[i];
+    }
+    for(int i=0;i<nasim.size();i++)
+    {
+        cout<<nasim[i]<< " ";
+    }
+}
