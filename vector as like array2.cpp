2b159c43d316355bd40ae421f773a975c9d8add4
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-04 10:32:10 +0600

    create the size when the vector declare

---
+++ vector as like array2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>nasim(5);
+
+    for(int i=0;i<nasim.size();i++)
+    {
+        cin>>nasim[i];
+    }
+    for(int i=0;i<nasim.size();i++)
+    {
+        cout<<nasim[i]<< " ";
+    }
+}
+
