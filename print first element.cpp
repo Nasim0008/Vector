854cd9563dab530cd028626408a7d8b5ee77bc1f
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-05 16:20:28 +0600

    print the first elements of the vector

---
+++ print first element.cpp
@@ -0,0 +1,18 @@
+//print the first elements of the vector
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>v={20,30,10,20,30};
+    cout<<*v.begin();//print the first element
+
+
+    return 0;
+}
+
+
+
+
+
