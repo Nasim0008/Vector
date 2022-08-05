8d3ed8a4cd77888d0e3d3df2dbf8a814dfb20dbb
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-05 15:50:54 +0600

    reverse of vector by using reverse function

---
+++ reverse.cpp
@@ -0,0 +1,19 @@
+//reverse of vector by using reverse function
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>v={20,30,10,20,30,1,2,3,4,56,25,36,10,12,26};
+    reverse(v.begin(),v.end());// those function can reverse from begin to end;
+    for(auto u:v)
+    {
+        cout<<u<< " ";
+    }
+
+
+
+}
+
+
