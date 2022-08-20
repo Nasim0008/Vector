921d081ebda9958bcce1f64f4ef4d0dd7c8a0be7
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-20 19:52:59 +0600

    add multiple string by using append function

---
+++ concetinate string.cpp
@@ -0,0 +1,10 @@
+//multiple string concatenate
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1="Md Nasim ",s2="Hossen";
+    s1+=s2;//string concatenate
+    cout<<s1<<endl;
+}
