101217aafc8bbbe8a9ff86a8d4fb2784d79f5275
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-10 18:15:21 +0600

    copy all the element from one vector to another

--- vector copy.cpp
+++ vector copy.cpp
@@ -1,20 +1,21 @@
-// copy the vector elements from one vector to another
-
 #include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    vector<int>nasim={12,25,36,78,96,45};
-    vector<int>temp;//empty vector
-    temp=nasim;//copy the elements from nasim vector
-
-    cout<<temp.size()<<endl;
-    //print the copied vector
-    for(int i=0;i<temp.size();i++)
+    int n;
+    cin>>n;
+    vector<int>v(n),ve;
+    for(int i=0;i<n;i++)
     {
-        cout<<temp[i]<< " ";
+        cin>>v[i];
     }
-
-   return 0;
+    for(int i=0;i<n;i++)
+    {
+        ve.push_back(v[i]);
+    }
+    for(auto u:ve)
+    {
+        cout<<u<< " ";
+    }
 }
+
