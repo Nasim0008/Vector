b53decce9aa99f00f0748174d6c4d97b0346e123
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-01 12:07:44 +0600

    check the vector is sorted or not

---
+++ check sorted.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   vector<int>s={10,20,30,40,50};
+   if(is_sorted(s.begin(),s.end()))
+   {
+       cout<< "Sorted"<<endl;
+   }
+   else
+   {
+       cout<< "Unsorted"<<endl;
+   }
+
+}
+
