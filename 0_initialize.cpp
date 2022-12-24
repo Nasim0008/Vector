0e3042b7ac4e6643588f84918614c5c0ef166550
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-24 20:38:02 +0600

    initialize the vector by the any value

---
+++ 0_initialize.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n,5);
+    for(auto u:v)
+    {
+        cout<<u<< " ";
+    }
+    cout<<endl;
+}
